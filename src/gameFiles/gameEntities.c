38e4877f1810fdb80f2b3c41e562a692cc34e439
Kapil3772 <k.asid721011@gmail.com>
2025-02-01 21:01:08 +0545

    organized code added entities.c file

---
+++ src/gameFiles/gameEntities.c
@@ -0,0 +1,111 @@
+#include "gameEntities.h"
+#include <stdio.h>
+
+float PLAYER_VELOCITY_X = 5;
+float PLAYER_VELOCITY_Y = 5;
+bool fps_flag = false;
+bool collision_flag[4] = {false, false, false, false};
+
+void updateEntity(PhysicsEntity *entity, bool movement[4])
+{
+    printf("ok");
+}
+
+void renderEntity(SDL_Renderer *renderer, PhysicsEntity *entity)
+{
+    printf("ok");
+}
+
+int top_collision(SDL_Rect rect1, SDL_Rect rect2)
+{
+    if ((rect1.y < rect2.y + rect2.h && rect1.x + rect1.w > rect2.x) && (rect1.y < rect2.y + rect2.h && rect1.x < rect2.x + rect2.w))
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+int right_collision(SDL_Rect rect1, SDL_Rect rect2)
+{
+    if ((rect1.x + rect1.w >= rect2.x) && (rect1.x <= rect2.x + rect2.w) && (rect1.y + rect1.h >= rect2.y) && (rect1.y <= rect2.y + rect2.h))
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+int left_collision(SDL_Rect rect1, SDL_Rect rect2)
+{
+    if (rect1.x < rect2.x + rect2.w && rect1.y + rect1.h > rect2.y && rect1.y < rect2.y + rect2.h)
+    {
+
+        return 1;
+        int top_overlap = rect2.y + rect2.h - rect1.y;    // Top side of rect2 colliding with rect1
+        int bottom_overlap = rect1.y + rect1.h - rect2.y; // Bottom side of rect2 colliding with rect1
+        int left_overlap = rect2.x + rect2.w - rect1.x;   // Left side of rect2 colliding with rect1
+        int right_overlap = rect1.x + rect1.w - rect2.x;  // Right side of rect2 colliding with rect1
+
+        // Determine the smallest overlap
+        int min_overlap = top_overlap;
+
+        if (bottom_overlap < min_overlap)
+        {
+            min_overlap = bottom_overlap;
+        }
+
+        if (left_overlap < min_overlap)
+        {
+            min_overlap = left_overlap;
+        }
+
+        if (right_overlap < min_overlap)
+        {
+        }
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+int bottom_collision(SDL_Rect rect1, SDL_Rect rect2)
+{
+    if ((rect1.y + rect1.h > rect2.y) && (rect1.x + rect1.w > rect2.x) && (rect1.x < rect2.x + rect2.w))
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+int bottom_window_collision(SDL_Rect rect1, SDL_Rect wrect)
+{
+    if ((rect1.y + rect1.h) > wrect.h + wrect.y)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+int side_window_collision(SDL_Rect rect1, SDL_Rect wrect)
+{
+    if ((rect1.x >= wrect.x) && (rect1.x + rect1.w <= wrect.x + wrect.w))
+    {
+        return 0;
+    }
+    else
+    {
+        return 1;
+    }
+}
--- src/gameFiles/gameEntities.h
+++ src/gameFiles/gameEntities.h
@@ -6,114 +6,35 @@
 #define PLAYER_HEIGHT 32
 #define GRAVITY 0.00
 #define WALL_THICKNESS 150
-#include "globals.h"
-#include "gameRender.h"
 
-float PLAYER_VELOCITY_X = 5;
-float PLAYER_VELOCITY_Y = 5;
+#include <stdbool.h>
+#include "SDL2/SDL.h"
 
-bool fps_flag = false;
+extern float PLAYER_VELOCITY_X;
+extern float PLAYER_VELOCITY_Y;
+
+extern bool fps_flag;
 
 // Physics Entities
-bool collision_flag[4] = {false, false, false, false};
+extern bool collision_flag[4];
+
+typedef struct
+{
+    float x, y;
+    const char type;
+    float w, h;
+    float velocity_x, velocity_y;
+} PhysicsEntity;
+
+void updateEntity(PhysicsEntity *entity, bool movement[4]);
+void renderEntity(SDL_Renderer *renderer, PhysicsEntity *entity);
 
 // Function to check collision between two SDL_Rects
 int top_collision(SDL_Rect, SDL_Rect);
-int top_collision(SDL_Rect rect1, SDL_Rect rect2)
-{
-    if ((rect1.y < rect2.y + rect2.h && rect1.x + rect1.w > rect2.x) && (rect1.y < rect2.y + rect2.h && rect1.x < rect2.x + rect2.w))
-    {
-        return 1;
-    }
-    else
-    {
-        return 0;
-    }
-}
-
 int right_collision(SDL_Rect, SDL_Rect);
-int right_collision(SDL_Rect rect1, SDL_Rect rect2)
-{
-    if ((rect1.x + rect1.w >= rect2.x) && (rect1.x <= rect2.x + rect2.w) && (rect1.y + rect1.h >= rect2.y) && (rect1.y <= rect2.y + rect2.h))
-    {
-        return 1;
-    }
-    else
-    {
-        return 0;
-    }
-}
-
 int left_collision(SDL_Rect, SDL_Rect);
-int left_collision(SDL_Rect rect1, SDL_Rect rect2)
-{
-    if (rect1.x < rect2.x + rect2.w && rect1.y + rect1.h > rect2.y && rect1.y < rect2.y + rect2.h)
-    {
-        return 1;
-    }
-    else
-    {
-        return 0;
-    }
-}
-
 int bottom_collision(SDL_Rect, SDL_Rect);
-int bottom_collision(SDL_Rect rect1, SDL_Rect rect2)
-{
-    if ((rect1.y + rect1.h > rect2.y) && (rect1.x + rect1.w > rect2.x) && (rect1.x < rect2.x + rect2.w))
-    {
-        return 1;
-    }
-    else
-    {
-        return 0;
-    }
-}
-
 int bottom_window_collision(SDL_Rect, SDL_Rect);
-int bottom_window_collision(SDL_Rect rect1, SDL_Rect wrect)
-{
-    if ((rect1.y + rect1.h) > wrect.h + wrect.y)
-    {
-        return 1;
-    }
-    else
-    {
-        return 0;
-    }
-}
-
 int side_window_collision(SDL_Rect, SDL_Rect);
-int side_window_collision(SDL_Rect rect1, SDL_Rect wrect)
-{
-    if ((rect1.x >= wrect.x) && (rect1.x + rect1.w <= wrect.x + wrect.w))
-    {
-        return 0;
-    }
-    else
-    {
-        return 1;
-    }
-}
-
-float get_dampness(float);
-float get_dampness(float dampness)
-{
-    int dampness_switch = 0;
-    if (dampness < GRAVITY)
-    {
-        dampness_switch = 1;
-    }
-    if (dampness_switch)
-    {
-        dampness += 1;
-    }
-    else
-    {
-        dampness = 0;
-    }
-
-    return dampness;
-}
 
 #endif
